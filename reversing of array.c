8a3b74bfbdd8cfc6f5be37be3f209df94b372b51
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-08-12 17:55:32 -0700

    Array reversal using for loop

---
+++ reversing of array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num, *arr, i;
+    printf("enter the required number of index value:");
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+    int temp;
+    for (i = 0; i < num / 2; i++) {
+        temp = (int) *(arr + num - i - 1);
+        *(arr + num - i - 1) = *(arr + i);
+        *(arr + i) = temp;
+    }    
+    
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
