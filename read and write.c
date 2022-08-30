d99d50a2579977866dd5bda83e587a924174a6c9
Puviyarasu Sakthivel <109471992+Puviyarasu140826@users.noreply.github.com>
2022-08-30 08:05:59 -0700

    Read and write an array using the pointer

---
+++ read and write.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+   int x[5], i;
+   int *pa;
+   pa = &x[0]; // or, pa = &x;
+
+   printf("Enter array element: ");
+   for(i=0;i<5;i++)
+   {
+     scanf("%d", (pa+i)); 
+   }
+
+   printf("Displaying Array: ");
+   for(i=0;i<5;i++)
+   {
+     printf("%d\t",*(pa+i));
+   }
+
+   printf("\n");
+
+   return 0;
+}
